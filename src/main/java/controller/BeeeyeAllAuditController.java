91faacbd076deba2ae247648ff3e8299d066ce24
zw9love <415789992@qq.com>
2018-04-14 14:47:51 +0800

    test annotation

--- src/main/java/controller/BeeeyeAllAuditController.java
+++ src/main/java/controller/BeeeyeAllAuditController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import annotation.MethodValidator;
 import com.jfinal.core.Controller;
 import com.jfinal.plugin.activerecord.Db;
 import com.jfinal.plugin.activerecord.Record;
@@ -8,65 +9,36 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import util.MyUtil;
 
+import javax.annotation.Resource;
+import java.lang.reflect.Method;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author zenngwei
  * @date 2018/04/11 12:31
  */
 public class BeeeyeAllAuditController extends Controller {
-    public static void main(String[] args) {
-        String host_ids = "5aab3228f266320f5b780415";
-        String sql = "select bpaa.time as time, bpaa.count as processAssess, bsaa.count as sensesAssess, bgfa.count as wbg_count, bfaa.count as attributePro from \n" +
-                "(\n" +
-                "\tSELECT FROM_UNIXTIME(time, '%m-%e') as time, COUNT(*) AS count \n" +
-                "\tFROM beeneedle_process_access_audit \n" +
-                "\tWHERE host_ids = '" + host_ids + "' and time BETWEEN UNIX_TIMESTAMP('2018-2-20 00:00:00') AND UNIX_TIMESTAMP('2018-3-20 00:00:00')\n" +
-                "\tGROUP BY FROM_UNIXTIME(time, '%Y%m%e') \n" +
-                ") as bpaa\n" +
-                "left join\n" +
-                "(\n" +
-                "\tSELECT FROM_UNIXTIME(time, '%m-%e') as time, COUNT(*) AS count \n" +
-                "\tFROM beeneedle_senses_access_audit \n" +
-                "\tWHERE host_ids = '" + host_ids + "' and time BETWEEN UNIX_TIMESTAMP('2018-3-1 00:00:00') AND UNIX_TIMESTAMP('2018-3-31 00:00:00')\n" +
-                "\tGROUP BY FROM_UNIXTIME(time, '%Y%m%e') \n" +
-                ") as bsaa\n" +
-                "on bpaa.time = bsaa.time \n" +
-                "left join\n" +
-                "(\n" +
-                "\tSELECT FROM_UNIXTIME(time, '%m-%e') as time, COUNT(*) AS count \n" +
-                "\tFROM beeneedle_gray_file_audit \n" +
-                "\tWHERE host_ids = '" + host_ids + "' and time BETWEEN UNIX_TIMESTAMP('2018-3-1 00:00:00') AND UNIX_TIMESTAMP('2018-3-31 00:00:00')\n" +
-                "\tGROUP BY FROM_UNIXTIME(time, '%Y%m%e') \n" +
-                ") as bgfa\n" +
-                "on bpaa.time = bgfa.time \n" +
-                "left join\n" +
-                "(\n" +
-                "\tSELECT FROM_UNIXTIME(time, '%m-%e') as time, COUNT(*) AS count \n" +
-                "\tFROM beeneedle_file_access_audit \n" +
-                "\tWHERE host_ids = '" + host_ids + "' and time BETWEEN UNIX_TIMESTAMP('2018-3-1 00:00:00') AND UNIX_TIMESTAMP('2018-3-31 00:00:00')\n" +
-                "\tGROUP BY FROM_UNIXTIME(time, '%Y%m%e') \n" +
-                ") as bfaa\n" +
-                "on bpaa.time = bfaa.time \n" +
-                "ORDER BY time;";
-        List<Record> list = Db.find(sql);
-        JSONArray postList = MyUtil.getRecordData(list);
-        for (int i = 0; i < postList.length(); i++) {
-            JSONObject jsonObject = postList.getJSONObject(i);
-            System.out.println(jsonObject);
-        }
 
+    @MethodValidator(name="get", age = 100)
+    public void doSomething() {
+    }
+//    @Resource
+    public static void main(String[] args) throws Exception{
+        Method method = BeeeyeAllAuditController.class.getMethod("doSomething", null);
+        // 可以判断出方法是是不是存在MyTarget的注解，能打印出这句话证明我们写的确实是一个注解
+        if (method.isAnnotationPresent(MethodValidator.class)) {
+            MethodValidator methodValidator = method.getAnnotation(MethodValidator.class);
+            System.out.println(methodValidator.name());
+            System.out.println(methodValidator.age());
+        }
     }
 
     public void get() throws JSONException {
         Map<String, Object> json = MyUtil.getJsonData(getRequest());
         String host_ids = MyUtil.getString(json, "host_ids");
         int dayCount = 30;
-        if(json.containsKey("date"))
+        if (json.containsKey("date"))
             dayCount = MyUtil.getInt(json, "date");
 
         int time = (int) Math.floor(System.currentTimeMillis() / 1000);
@@ -175,7 +147,10 @@ public class BeeeyeAllAuditController extends Controller {
             dataList.put(dataObject);
         }
 
-        JSONObject jsonObj = MyUtil.getJson("成功", 200, dataList);
+        List<Object> dataReverseList = dataList.toList();
+        Collections.reverse(dataReverseList);
+
+        JSONObject jsonObj = MyUtil.getJson("成功", 200, dataReverseList);
         renderJson(jsonObj.toString());
     }
 }
